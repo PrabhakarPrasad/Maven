d7c3c7eb71ad3e445547d1b47c0d0ba6c9e5cbee
Prabhakar Prasad <prabhakarp300@gmail.com>
2018-01-22 00:11:48 +0530

    new class is added

---
+++ src/test/java/com/maven/selenium/Git_Confirmation.java
@@ -0,0 +1,11 @@
+package com.maven.selenium;
+
+import org.testng.annotations.Test;
+
+public class Git_Confirmation {
+	@Test
+	public void test1(){
+		System.out.println("Added new Java class");
+	}
+
+}
