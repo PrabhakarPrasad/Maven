ac712732230cdbadb44587e55276ed3119967f0b
Prabhakar Prasad <prabhakarp300@gmail.com>
2018-01-21 22:47:42 +0530

    Added full project

---
+++ src/test/java/com/maven/selenium/Maven_Jenkins.java
@@ -0,0 +1,11 @@
+package com.maven.selenium;
+
+import org.testng.annotations.Test;
+
+public class Maven_Jenkins {
+	@Test
+	public void Test1(){
+		System.out.println("Welcome to Jenkins world");
+	}
+
+}
---
+++ src/test/java/com/maven/selenium/MultiWindow.java
@@ -0,0 +1,40 @@
+package com.maven.selenium;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.interactions.Actions;
+
+import org.testng.annotations.Test;
+
+
+
+public class MultiWindow {
+
+	@Test 
+	public static void t1() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://demoqa.com/frames-and-windows/");
+		//driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='tabs-1']/div/p/a")).click();
+		driver.findElement(By.xpath(".//*[@id='tabs-1']/div/p/a ")).click();
+		Set <String> allwindow=driver.getWindowHandles();
+		Iterator <String>it= allwindow.iterator();
+		String ParentWin= it.next();
+        driver.switchTo().window(ParentWin);
+        driver.findElement(By.xpath(".//*[@id='menu-item-66']/a")).click();
+        Actions act=new Actions(driver);
+        act.moveToElement(driver.findElement(By.xpath(".//*[@id='menu-item-153']/a"))).click().perform(); 
+
+	}
+
+
+
+}
